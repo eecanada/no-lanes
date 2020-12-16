be9356bf0cbb59a2ef2a046589d65d1d01a5f32f
Eder Canada <canada.eder@gmail.com>
2020-12-15 16:05:56 -0800

    layout for songs

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-alert("Hello! I am an alert box!!");
+// let soundCloudLogo = document.querySelector('.soundHeader__rightRow')
+// soundCloudLogo.style.display = 'none'
+
+let soundCloudLogoo = document.getElementsByClassName('.music')[0]
+soundCloudLogoo.style.display = 'none';
