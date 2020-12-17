80976b672ea368f694e22d356941015fb1a5f394
Eder Canada <canada.eder@gmail.com>
2020-12-16 17:16:39 -0800

    video up

--- index.js
+++
@@ -1,5 +0,0 @@
-// let soundCloudLogo = document.querySelector('.soundHeader__rightRow')
-// soundCloudLogo.style.display = 'none'
-
-let soundCloudLogoo = document.getElementsByClassName('.music')[0]
-soundCloudLogoo.style.display = 'none';
